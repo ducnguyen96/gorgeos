7b62f78d8f167d6405dc3b26999c87d867c717bd
ducnguyen96 <levinguyen.dl@gmail.com>
2024-01-11 00:55:50 +0700

    add fonts

--- modules/system/misc/default.nix
+++ modules/system/misc/default.nix
@@ -1,2 +1,23 @@
-{
+{pkgs, ...}: {
+  fonts = {
+    packages = with pkgs; [
+      material-icons
+      material-design-icons
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      roboto
+
+      (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
+    ];
+
+    enableDefaultPackages = false;
+
+    fontconfig.defaultFonts = {
+      serif = ["Noto Serif" "Noto Color Emoji"];
+      sansSerif = ["Dosis" "Noto Color Emoji"];
+      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+      emoji = ["Noto Color Emoji"];
+    };
+  };
 }
