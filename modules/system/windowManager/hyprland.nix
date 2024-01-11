c610d3f51d54d0c2744daba9cc7027e2cd6baa65
ducnguyen <levinguyen.dl@gmail.com>
2024-01-11 15:16:23 +0700

    feaT: add greetd

--- modules/system/windowManager/hyprland.nix
+++ modules/system/windowManager/hyprland.nix
@@ -51,5 +51,21 @@
     };
   };
 
+  services = {
+    greetd = let
+      session = {
+        command = "${lib.getExe config.programs.hyprland.package}";
+        user = "duc";
+      };
+    in {
+      enable = true;
+      settings = {
+        terminal.vt = 1;
+        default_session = session;
+        initial_session = session;
+      };
+    };
+  };
+
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
 }
