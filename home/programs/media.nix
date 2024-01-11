64e9e4c794ecd22664fcf043b7d89ebe1a8b48bc
ducnguyen <levinguyen.dl@gmail.com>
2024-01-11 10:55:40 +0700

    feat: add mpv

--- home/programs/media.nix
+++ home/programs/media.nix
@@ -4,6 +4,18 @@
   ];
 
   programs = {
+    mpv = {
+      enable = true;
+      bindings = {
+        UP = "add volume +2";
+        DOWN = "add volume -2";
+      };
+      config = {
+        hwdec = "auto";
+        border = false;
+      };
+    };
+
     obs-studio = {
       enable = true;
       plugins = with pkgs.obs-studio-plugins; [wlrobs];
