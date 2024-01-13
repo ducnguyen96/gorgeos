58d7a0ba7d57860871996dca18b6350c0a8e060e
ducnguyen <levinguyen.dl@gmail.com>
2024-01-13 15:29:54 +0700

    chore: add legacy nodejs devShell flake, nodejs devShell flake

--- shells/legacy-nodejs.nix
+++ shells/legacy-nodejs.nix
@@ -1,5 +1,5 @@
 {
-  description = "nodejs_20 devShell";
+  description = "legacy nodejs-16_x devShell";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
@@ -12,13 +12,14 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import (builtins.fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/53951c0c1444e500585205e8b2510270b2ad188f.tar.gz";
+          sha256 = "1zybhkqf9smga3px6fcbhxqzfn1d5l1x30zjg4l9nqgn3419f4r2";
+        }) {system = system;};
       in rec {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            nodejs_20
+            nodejs-16_x
             nodePackages.yarn
           ];
 
