7f8facbdb8ca54d210224681dd08ba0116368631
ducnguyen <levinguyen.dl@gmail.com>
2024-01-21 22:34:34 +0700

    chore: update

--- home/programs/neovim/config/lua/plugins/ui/colorscheme.lua
+++ home/programs/neovim/config/lua/plugins/ui/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  { "catppuccin/nvim" },
+	{ "catppuccin/nvim" },
 
-  -- Configure LazyVim to load catppuccin
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	-- Configure LazyVim to load catppuccin
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin",
+		},
+	},
 }
