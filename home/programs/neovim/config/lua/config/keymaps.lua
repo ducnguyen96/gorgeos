da20f6c4398bb2647e2cfeff7d185ad284edfb96
ducnguyen <levinguyen.dl@gmail.com>
2024-01-21 14:02:07 +0700

    chore: always show bufferline

--- home/programs/neovim/config/lua/config/keymaps.lua
+++ home/programs/neovim/config/lua/config/keymaps.lua
@@ -8,32 +8,37 @@ local set = vim.keymap.set
 harpoon:setup()
 
 -- harpoon keymaps
-set("n", "<leader>a", function()
+set("n", "<leader>ha", function()
 	harpoon:list():append()
-end)
-set("n", "<C-e>", function()
+end, { desc = "Add current file to harpoon", silent = true })
+
+set("n", "<leader>hh", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
-end)
+end, { desc = "Show harpoon menu", silent = true })
 
-set("n", "<C-h>", function()
+set("n", "<leader>hq", function()
 	harpoon:list():select(1)
-end)
-set("n", "<C-t>", function()
-	harpoon:list():select(2)
-end)
-set("n", "<C-n>", function()
-	harpoon:list():select(3)
-end)
-set("n", "<C-s>", function()
-	harpoon:list():select(4)
-end)
+end, { desc = "Select harpoon item 1", silent = true })
 
-set("n", "<C-S-P>", function()
+set("n", "<leader>hw", function()
+	harpoon:list():select(2)
+end, { desc = "Select harpoon item 2", silent = true })
+
+set("n", "<leader>he", function()
+	harpoon:list():select(3)
+end, { desc = "Select harpoon item 3", silent = true })
+
+set("n", "<leader>hr", function()
+	harpoon:list():select(4)
+end, { desc = "Select harpoon item 4", silent = true })
+
+set("n", "<leader>hp", function()
 	harpoon:list():prev()
-end)
-set("n", "<C-S-N>", function()
+end, { desc = "Select harpoon prev item", silent = true })
+
+set("n", "<leader>hn", function()
 	harpoon:list():next()
-end)
+end, { desc = "Select harpoon next item", silent = true })
 
 -- window keymaps
 set("n", "<leader>wv", "<C-W>v", { desc = "Split window vertically", remap = true })
@@ -41,8 +46,9 @@ set("n", "<leader>wh", "<C-W>s", { desc = "Split window horizontally", remap = t
 
 -- editor keymaps
 set("i", "<C-BS>", "<C-W>", { desc = "Delete word backwards", silent = true })
-set("n", "<leader>cs", "<cmd>noautocmd w<cr>", { desc = "Save without formatting", silent = true })
 set("n", "<leader>cs", "<cmd>SymbolsOutline<cr>", { desc = "Show symbols outline", silent = true })
 
 -- code keymaps
 set("n", "<leader>o", "o<ESC>", { desc = "Insert new line below", silent = true })
+set("n", "<leader>sf", "<cmd>noautocmd w<cr>", { desc = "Save, format", silent = true })
+set("n", "<leader>snf", "<cmd>noautocmd w<cr>", { desc = "Save no format", silent = true })
