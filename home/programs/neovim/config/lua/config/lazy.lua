e00e1d9b19c233cedbffe07096891a3e37950982
ducnguyen <levinguyen.dl@gmail.com>
2024-02-24 09:09:58 +0700

    feat: add terraform

--- home/programs/neovim/config/lua/config/lazy.lua
+++ home/programs/neovim/config/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.util.project" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
+		{ import = "lazyvim.plugins.extras.lang.terraform" },
 		-- import/override with your plugins
 		{ import = "plugins.editor" },
 		{ import = "plugins.formatting" },
