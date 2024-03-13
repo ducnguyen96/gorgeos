401dc328f096f97a89402523bfc5aecd339eba9f
ducnguyen <levinguyen.dl@gmail.com>
2024-03-13 16:30:32 +0700

    chore: add new firefox profile

--- home/programs/firefox.nix
+++ home/programs/firefox.nix
@@ -67,5 +67,40 @@ in {
 
       extraConfig = builtins.readFile "${firefox-gnome-theme}/share/firefox-gnome-theme/configuration/user.js";
     };
+
+    profiles.cyberpredictit = {
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+      ];
+
+      settings = {
+        "browser.tabs.loadInBackground" = true;
+        "gfx.canvas.accelerated" = true;
+        "gfx.webrender.enabled" = true;
+        "gfx.x11-egl.force-enabled" = true;
+        "layers.acceleration.force-enabled" = true;
+        "media.av1.enabled" = false;
+        "media.ffmpeg.vaapi.enabled" = true;
+        "media.hardware-video-decoding.force-enabled" = true;
+        "media.rdd-ffmpeg.enabled" = true;
+        "widget.dmabuf.force-enabled" = true;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        "gnomeTheme.hideSingleTab" = true;
+        "gnomeTheme.bookmarksToolbarUnderTabs" = true;
+        "gnomeTheme.normalWidthTabs" = false;
+        "gnomeTheme.tabsAsHeaderbar" = false;
+      };
+
+      userChrome = ''
+        @import "${firefox-gnome-theme}/share/firefox-gnome-theme/userChrome.css";
+      '';
+
+      userContent = ''
+        @import "${firefox-gnome-theme}/share/firefox-gnome-theme/userContent.css";
+      '';
+
+      extraConfig = builtins.readFile "${firefox-gnome-theme}/share/firefox-gnome-theme/configuration/user.js";
+    };
   };
 }
