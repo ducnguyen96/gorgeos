f8504d5ae7f71ccb3cd92e362057e0e420e8fa55
ducnguyen <levinguyen.dl@gmail.com>
2024-04-28 10:13:44 +0700

    chore: remove authy, php, firebase-tools

--- home/dev/default.nix
+++ home/dev/default.nix
@@ -5,13 +5,10 @@
 
   home.packages = with pkgs; [
     pritunl-client
-    authy
     dbeaver
 
     nodePackages.yarn
     nodePackages.pnpm
-    nodePackages.firebase-tools
-    php
     go
   ];
 }
