da8a0404290d897c379e9e175c9ffd1d015d7a94
duc.nguyen <duc.nguyen@forwoodsafety.com>
2024-07-23 10:18:28 +0700

    fix: conform warning

--- home/modules/programs/neovim/config/lua/plugins/conform.lua
+++ home/modules/programs/neovim/config/lua/plugins/conform.lua
@@ -2,12 +2,11 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		-- LazyVim will use these options when formatting with the conform.nvim formatter
-		format = {
+		default_format_opts = {
 			timeout_ms = 3000,
 			async = false, -- not recommended to change
 			quiet = false, -- not recommended to change
 		},
-		---@type table<string, conform.FormatterUnit[]>
 		formatters_by_ft = {
 			lua = { "stylua" },
 			fish = { "fish_indent" },
@@ -25,9 +24,6 @@ return {
 			tsx = { "prettier" },
 			jsx = { "prettier" },
 		},
-		-- The options you set here will be merged with the builtin formatters.
-		-- You can also define any custom formatters here.
-		---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
 		formatters = {
 			injected = { options = { ignore_errors = true } },
 			-- # Example of using dprint only when a dprint.json file is present
