2dbd0ff736c15308b2e633612b4c6ca8170674b7
ducnguyen96 <levinguyen.dl@gmail.com>
2024-07-24 17:32:25 +0700

    chore: add symbols-outline

--- home/modules/programs/neovim/config/lua/plugins/lsp.lua
+++ home/modules/programs/neovim/config/lua/plugins/lsp.lua
@@ -13,4 +13,13 @@ return {
 			ensure_installed = {},
 		},
 	},
+	{
+		"simrat39/symbols-outline.nvim",
+		cmd = "SymbolsOutline",
+		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+		opts = {
+			-- add your options that should be passed to the setup() function here
+			position = "right",
+		},
+	},
 }
