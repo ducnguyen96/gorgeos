a9cb08cdd974c7229c415ff80976392b1c8c347f
ducnguyen96 <levinguyen.dl@gmail.com>
2024-08-31 21:42:52 +0700

    chore: remove subliminal

--- home/modules/programs/media/mpv.nix
+++ home/modules/programs/media/mpv.nix
@@ -12,18 +12,6 @@
         sha256 = "sha256-HqZyA7cIlFeg6bJfyENab8O+iCfxZRmMqEnQpgsukC8=";
       };
     };
-  subliminal = let
-    pname = "subliminal";
-    version = "2.2.1";
-    format = "wheel";
-  in
-    pythonPackages.buildPythonPackage {
-      inherit pname version format;
-      src = pkgs.fetchurl {
-        url = "https://files.pythonhosted.org/packages/09/d1/cddf58f2556af8c6b353497cdd0e39efe4b75ae50f9e8cfdb7476e62c2f5/subliminal-2.2.1-py3-none-any.whl";
-        sha256 = "sha256-Qhpx8uP2BOXf+1UbKlHRRQDHYV1+rxbCPnE9etKVUEw=";
-      };
-    };
 in {
   programs.mpv = {
     enable = true;
@@ -40,11 +28,7 @@ in {
 
   home.packages = [
     pkgs.mov-cli
-    subliminal
     film-central
-    pythonPackages.lxml
-    pythonPackages.dogpile-cache
-
     pkgs.yt-dlp
   ];
 
