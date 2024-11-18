fa43d00b8ea8ff66b952a8a1bf5fe4e1ec00c832
ducnguyen96 <levinguyen.dl@gmail.com>
2024-11-18 16:52:01 +0700

    feat: use ducnguyen96 cachix

--- hosts/modules/config/nix/substituters.nix
+++ hosts/modules/config/nix/substituters.nix
@@ -5,6 +5,7 @@
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
+      "https://ducnguyen96.cachix.org"
     ];
 
     trusted-public-keys = [
@@ -12,6 +13,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "ducnguyen96.cachix.org-1:0Dr87meLg1d8ag+V7a0VYckB6uBwWcmNyW5zXpaO1xY="
     ];
   };
 }
--- hosts/profiles/rpi4/configuration.nix
+++ hosts/profiles/rpi4/configuration.nix
@@ -4,6 +4,7 @@
   raspberry-pi-nix.board = "bcm2711";
 
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  hardware.enableRedistributableFirmware = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
