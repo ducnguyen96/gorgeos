4d83386d1d3cfe40f951111f1fbe637de228d7e9
ducnguyen96 <levinguyen.dl@gmail.com>
2024-11-20 16:10:25 +0700

    chore: rpi4 add shell module

--- home/profiles/duc-rpi4.nix
+++ home/profiles/duc-rpi4.nix
@@ -1,4 +1,27 @@
 {
+  imports = [
+    ../modules/dev/default.nix
+    ../modules/programs/neovim
+    ../modules/programs/nix-index-db.nix
+
+    # shell module
+    ../modules/shell/programs/bat.nix
+    ../modules/shell/programs/dircolors.nix
+    ../modules/shell/programs/direnv.nix
+    ../modules/shell/programs/eza.nix
+    ../modules/shell/programs/fzf.nix
+    ../modules/shell/programs/git.nix
+    ../modules/shell/programs/htop.nix
+    ../modules/shell/programs/ranger.nix
+    ../modules/shell/programs/starship.nix
+    ../modules/shell/programs/zoxide.nix
+    ../modules/shell/scripts/clean.nix
+    ../modules/shell/scripts/rebuild.nix
+    ../modules/shell/services/ssh.nix
+    ../modules/shell/zsh.nix
+    ../modules/shell/utils.nix
+  ];
+
   home = {
     username = "duc";
     homeDirectory = "/home/duc";
