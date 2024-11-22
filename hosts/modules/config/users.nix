8f88e37370036ef323fc227c7db43dd22af0e254
ducnguyen96 <levinguyen.dl@gmail.com>
2024-11-22 10:01:03 +0700

    chore: add ssh key to root user

--- hosts/modules/config/users.nix
+++ hosts/modules/config/users.nix
@@ -20,10 +20,15 @@
       "wheel"
       "i2c"
       "wireshark"
+      "cloudflared"
     ];
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGNbn8ULX8j+jDdAEUkn9++ol6QR57rrpTURo7FcaBF duc@master"
     ];
   };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGNbn8ULX8j+jDdAEUkn9++ol6QR57rrpTURo7FcaBF duc@master"
+  ];
 }
