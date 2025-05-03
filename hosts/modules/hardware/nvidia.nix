e324674ed8e7bf6328d63b8c13768f99d89897b5
duc.nguyen <levinguyen.dl@gmail.com>
2025-05-03 15:29:40 +0700

    chore: update substituters

--- hosts/modules/hardware/nvidia.nix
+++ hosts/modules/hardware/nvidia.nix
@@ -36,4 +36,14 @@ in {
 
     services.xserver.videoDrivers = ["nvidia"];
   };
+
+  nix.settings = {
+    substituters = [
+      "https://cuda-maintainers.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
+  };
 }
