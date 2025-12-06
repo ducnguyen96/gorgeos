099bf6b9a59de0aa709e1b963b87f316a4c2c785
ducnguyen96 <levinguyen.dl@gmail.com>
2025-12-06 16:15:29 +0000

    chore: update cloud hardware config

--- hosts/profiles/cloud/hardware-configuration.nix
+++ hosts/profiles/cloud/hardware-configuration.nix
@@ -1,4 +1,4 @@
-{ modulesPath, lib, ... }:
+{ modulesPath, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot.loader.grub = {
@@ -10,5 +10,5 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";  
+  
 }
